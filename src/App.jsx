d13de65305b188b98d3fbbafa4838bd475669e80
Adaobilynda1234 <adaobilynda1234@gmail.com>
2024-06-20 20:44:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center text-blue-500">
+        Trakmama!
+      </h1>
+    </>
+  );
+}
+
+export default App;
