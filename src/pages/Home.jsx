e2dc6fc104e8853307f66ed2c403579fc7c5011d
Omotola Sanni <omotola302@gmail.com>
2024-06-27 18:04:15 +0100

    add button display hidden on small screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Herocarousel from "../components/Herocarousel";
 import Review from "../components/Review";
+import Articles from "../components/articles";
+import OurTools from "../components/ourtools";
 
 const Home = () => {
   return (
     <div>
       <Herocarousel />
+      <Articles />
+      <OurTools />
       {/* <h1 className="text-center">this is home page</h1> */}
       <Review />
     </div>
