523b60a4286bc3555f532f9b4388f563618d0dcd
cynthia-creator <ceemaruta@gmail.com>
2024-06-27 18:06:38 +0200

    CLOSING HOME.JSX

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-//import React from 'react'
+import React from 'react'
 import logo2 from '../assets/logo2.png';
 
 const Footer = ()=>{
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
-//import React from "react";
+import React from "react";
 //import Herocarousel from "../components/Herocarousel";
 //import Review from "../components/Review";
 
 const Home = () => {
   return (
     <div>
-      <Herocarousel />
+      {/*<Herocarousel />*/}
 
        <h1 className="text-center">this is home page</h1>
-      <Review />
+      {/* <Review /> */}
     </div>
-  );
+  )
 
+}
 
 export default Home
