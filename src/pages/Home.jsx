53f5603cfac1aa3026d8e16cc556a1abd0519910
cynthia-creator <ceemaruta@gmail.com>
2024-06-27 17:47:05 +0200

    added footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import Herocarousel from "../components/Herocarousel";
-import Review from "../components/Review";
+//import React from "react";
+//import Herocarousel from "../components/Herocarousel";
+//import Review from "../components/Review";
 
 const Home = () => {
   return (
     <div>
       <Herocarousel />
-      {/* <h1 className="text-center">this is home page</h1> */}
+
+       <h1 className="text-center">this is home page</h1>
       <Review />
     </div>
   );
-};
 
-export default Home;
+
+export default Home
