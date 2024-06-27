8e789bd8d4474b4a9b14903f82cb93ae260899a1
Adaobilynda1234 <adaobilynda1234@gmail.com>
2024-06-27 17:45:17 +0100

    merged articles and review

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Herocarousel from "../components/Herocarousel";
+import Article from "../components/articles";
+import Ourtools from "../components/ourtools";
 import Review from "../components/Review";
 
 const Home = () => {
   return (
     <div>
       <Herocarousel />
+      <Article />
+      <Ourtools />
       {/* <h1 className="text-center">this is home page</h1> */}
       <Review />
     </div>
