02276a06c46019f227e4dce3d80c7bd1dc53aa18
cynthia-creator <ceemaruta@gmail.com>
2024-07-03 17:35:37 +0200

    about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,9 +4,19 @@ import Mission from "../components/Mission";
 export const AboutUs = () => {
   return (
     <div>
-      <h1 className="text-center">this is About us page</h1>
+      
       <Mission/>
     </div>
+    <div>  
+      <Whatevalue/>
+      <br/>
+      <Disclaimer/>
+
+      
+     
+      
+      
+      </div>
   );
 };
 
