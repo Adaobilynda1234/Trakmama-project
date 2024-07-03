22df96f9734759b04ce1d0ddfa3eb80f5de37128
cynthia-creator <ceemaruta@gmail.com>
2024-07-03 21:52:10 +0200

    Updated disclaimer

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -5,9 +5,9 @@ const Disclaimer =()=>{
     return(
         
       <div className="    ">
-      <h3 className=" place-items-baseline  pl-14 pt-12 text-lg  mb-0 font-medium text-black  font-sans">Disclaimer</h3> 
+      <h3 className="  text-lg  text-center mb-0 font-medium text-black  font-sans">Disclaimer</h3> 
               
-        <p className="text-center  m-10 w-1/2 h-auto  flex-wrap justify-start md:items-center text-gray-600 text-lg font-sans mt-0">Trakmama A.I is not always accurate only. Consult with a medical professional 
+        <p className="   text-center  md:items-center text-gray-600 text-lg font-sans mt-0">Trakmama A.I is not always accurate only. <pre/>Consult with a medical professional 
             if you have health concerns. For more information, see our 
             <a href="/" className="text-black-500  underline hover:underline-">  Terms of Use.</a> </p> 
        
